02089845e22bebb50b20273c98bd0ac07b28701c
Yakini942 <y.beni250@gmail.com>
2025-08-13 18:48:46 +0200

    nwe commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-
 import HomeContent from './HomeContent';
 import { fetchStrapi } from '@/lib/strapi';
 
 export const revalidate = 60;
 
 export default async function Home() {
-  const [hero,  testimonials] = await Promise.all([
+  const [heroData, testimonialsData] = await Promise.all([
     fetchStrapi('hero'),
     fetchStrapi('testimonials')
   ]);
 
+  // Provide defaults in case data is missing
+  const hero = heroData || {};
+  const testimonials = testimonialsData || [];
+
   return <HomeContent hero={hero} testimonials={testimonials} />;
 }
-
