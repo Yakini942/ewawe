23245cc0bd1783a7f9585d805709f62df6fbbf65
Yakini942 <y.beni250@gmail.com>
2025-08-18 09:01:13 +0200

    qf

--- app/heroSection.tsx
+++ app/heroSection.tsx
@@ -30,7 +30,7 @@ const slides: Slide[] = [
     text: 'Shop confidently with ESL Money Back Guarantee',
   },
   {
-    image: '/hero/slide 4.png',
+    image: '/hero/slide 4.jpgcv',
     title: 'Ewawe Shippers Limited',
     subtitle: 'Global Shipping Made Easy',
     text: 'FAST, RELIABLE & AFFORDABLE',
